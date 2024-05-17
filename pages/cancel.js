6a4e827a220a6db70940aa788a5dc832b231aaba
satvinderladdi1020 <satvinderladdi1020@gmail.com>
2024-05-17 18:43:16 +0530

    stripe setup

---
+++ pages/cancel.js
@@ -0,0 +1,7 @@
+// pages/cancel.js
+const Cancel = () => {
+    return <h1>Payment Canceled</h1>;
+  };
+  
+  export default Cancel;
+  
---
+++ pages/success.js
@@ -0,0 +1,7 @@
+// pages/success.js
+const Success = () => {
+    return <h1>Payment Successful!</h1>;
+  };
+  
+  export default Success;
+  
